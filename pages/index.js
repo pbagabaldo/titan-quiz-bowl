ee35f517655f1379f306fc67ea1d1cb89f5a8c56
janiljohn <56321852+janiljohn@users.noreply.github.com>
2022-10-14 11:01:57 -0700

    Updated home page to include different categories

--- pages/index.js
+++ pages/index.js
@@ -13,41 +13,30 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Jesus <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Titan Quiz Bowl!
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="/posts/music" className={styles.card}>
+            <h2>Music &rarr;</h2>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2>Geography &rarr;</h2>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>History &rarr;</h2>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h2>General &rarr;</h2>
           </a>
         </div>
       </main>
