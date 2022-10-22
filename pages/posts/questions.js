5f74261d3a5230795c28a3a9230fd5fa92d64474
JonAguirre0 <103548831+JonAguirre0@users.noreply.github.com>
2022-10-22 09:25:58 -0700

    Created questions for each category (#9)
    * initial commit
    
    * Created questions for each category
    
    Co-authored-by: Joel Anil John <56321852+janiljohn@users.noreply.github.com>

---
+++ pages/posts/questions.js
@@ -0,0 +1,340 @@
+var Musicquestions=[
+    {
+        id:1,
+        question: "Which percussion instrument is named after its shape?", 
+        answerOptions: {
+            a:"Circle",
+            b:"Square",
+            c:"Triangle", 
+            d:"Hexagon",
+        },
+        correctAnswer: 'c'
+    },
+    {
+        id:2,
+        question: "The flute is a member of the ___.", 
+        answerOptions: {
+            a:"Brass Family",
+            b:"Woodwind Family", 
+            c:"Percussion Family",
+            d:"String Family",
+        },
+        correctAnswer: 'b'
+    },
+    {
+        id:3,
+        question: "Which of these instruments belongs in the string family?", 
+        answerOptions: {
+            a:"Oboe",
+            b:"Piccolo",
+            c:"Cello", 
+            d:"Tambourine",
+        },
+        correctAnswer: 'c'
+    },
+    {
+        id:4,
+        question: "Mozart, Beethoven and Bach were all famous...",
+        answerOptions: {
+            a: "Kings",
+            b: "Knights",
+            c: "Composers",
+            d: "Magicans"
+        },
+        correctAnswer: 'c'
+    },
+    {
+        id:5,
+        question: "A huge band from the 1960s was called The Rolling...",
+        answerOptions: {
+            a: "Wheels",
+            b: "Wagons",
+            c: "Pins",
+            d: "Stones"
+        },
+        correctAnswer: 'd'
+    },
+    {
+        id:6,
+        question: "Which of these instruments does not have strings?",
+        answerOptions: {
+            a: "Piano",
+            b: "Bass guitar",
+            c: "Drum",
+            d: "Harp"
+        },
+        correctAnswer: 'c'
+    },
+    {
+        id:7,
+        question: "Which of the following describes playing a poece of music alone?",
+        answerOptions: {
+            a: "Duet",
+            b: "Symphony",
+            c: "Solo",
+            d: "Quartet"
+        },
+        correctAnswer: 'c'
+    },
+    {
+        id:8,
+        question: "In an Orchestra who directs the musicians?",
+        answerOptions: {
+            a: "The manager",
+            b: "The conductor",
+            c: "The main musician",
+            d: "The best musician"
+        },
+        correctAnswer: 'b'
+    },
+    {
+        id:9,
+        question: "How many musicians are in a duet?",
+        answerOptions: {
+            a: "One",
+            b: "Two",
+            c: "Three",
+            d: "Four"
+        },
+        correctAnswer: 'b'
+    },
+    {
+        id:10,
+        question: "Which member of an orchestra holds a baton?",
+        answerOptions: {
+            a: "Conductor",
+            b: "Percussionist",
+            c: "French Horn Player",
+            d: "Violinist"
+        },
+        correctAnswer: 'a'
+    }
+]
+
+
+var Geographyquestions=[
+    {
+        id:1,
+        question: "Which is the tallest mountain on Earth?", 
+        answerOptions: {
+            a:"Mount Everest",
+            b:"Mount Washington",
+            c:"Mount Whitney", 
+            d:"Mount St. Helens",
+        },
+        correctAnswer: 'a'
+    },
+    {
+        id:2,
+        question: "What are areas of the Earth that are bigger than just countries called?", 
+        answerOptions: {
+            a:"Regions",
+            b:"Counties", 
+            c:"Islands",
+            d:"Continents",
+        },
+        correctAnswer: 'd'
+    },
+    {
+        id:3,
+        question: "Which is the biggest continent?", 
+        answerOptions: {
+            a:"Africa",
+            b:"Europe",
+            c:"Asia", 
+            d:"Australia",
+        },
+        correctAnswer: 'c'
+    },
+    {
+        id:4,
+        question: "Which of these countries is a group of islands off the coast of Asia?",
+        answerOptions: {
+            a: "Britain",
+            b: "Germany",
+            c: "Spain",
+            d: "Japan"
+        },
+        correctAnswer: 'd'
+    },
+    {
+        id:5,
+        question: "Which is the largest country in the world?",
+        answerOptions: {
+            a: "Russia",
+            b: "China",
+            c: "North America",
+            d: "India"
+        },
+        correctAnswer: 'a'
+    },
+    {
+        id:6,
+        question: "Which is the largest desert in the world?",
+        answerOptions: {
+            a: "Arabian",
+            b: "Sahara",
+            c: "Gobi",
+            d: "Mojave"
+        },
+        correctAnswer: 'b'
+    },
+    {
+        id:7,
+        question: "Which is the longest river in the world?",
+        answerOptions: {
+            a: "Amazon",
+            b: "Mississippi",
+            c: "Missouri",
+            d: "Nile"
+        },
+        correctAnswer: 'd'
+    },
+    {
+        id:8,
+        question: "What is the largest US state by land area?",
+        answerOptions: {
+            a: "California",
+            b: "Alaska",
+            c: "Texas",
+            d: "Hawaii"
+        },
+        correctAnswer: 'b'
+    },
+    {
+        id:9,
+        question: "What is the capital city of Japan?",
+        answerOptions: {
+            a: "Berlin",
+            b: "Tokyo",
+            c: "Dublin",
+            d: "Paris"
+        },
+        correctAnswer: 'b'
+    },
+    {
+        id:10,
+        question: "What is the largest natural rainforst in the world?",
+        answerOptions: {
+            a: "The Amazon Rainforest",
+            b: "The Congo Rainforest",
+            c: "Southeast Asian Rainforest",
+            d: "Daintree Rainforest"
+        },
+        correctAnswer: 'a'
+    }
+]
+
+
+var Historyquestions=[
+    {
+        id:1,
+        question: "Who was the first American President?", 
+        answerOptions: {
+            a:"George Washington",
+            b:"James Madison",
+            c:"Thomas Jefferson", 
+            d:"Abraham Lincoln",
+        },
+        correctAnswer: 'a'
+    },
+    {
+        id:2,
+        question: "On which city was the first atomic bomb attack?", 
+        answerOptions: {
+            a:"Tokyo",
+            b:"Hiroshima", 
+            c:"Osaka",
+            d:"Kyoto",
+        },
+        correctAnswer: 'b'
+    },
+    {
+        id:3,
+        question: "Which era marked a switch from agricultural practies to industrial practices?", 
+        answerOptions: {
+            a:"The Great Depression",
+            b:"The Middle Ages",
+            c:"Victorian Era", 
+            d:"The Industrial Revolution",
+        },
+        correctAnswer: 'd'
+    },
+    {
+        id:4,
+        question: "Who was the president during the Cuban Missile Crisis?",
+        answerOptions: {
+            a: "John F. Kennedy",
+            b: "Theodore Roosevelt",
+            c: "James Madison",
+            d: "George Washington"
+        },
+        correctAnswer: 'a'
+    },
+    {
+        id:5,
+        question: "What was the name of the ancient trade route that connected the East with the west?",
+        answerOptions: {
+            a: "The Silk Road",
+            b: "The Spice Route",
+            c: "The Salt Route",
+            d: "The Tin Route"
+        },
+        correctAnswer: 'a'
+    },
+    {
+        id:6,
+        question: "Which fabric was invented in Ancient China?",
+        answerOptions: {
+            a: "Denim",
+            b: "Cotton",
+            c: "Linen",
+            d: "Silk"
+        },
+        correctAnswer: 'd'
+    },
+    {
+        id:7,
+        question: "What is the world's oldest recorded civilization?",
+        answerOptions: {
+            a: "Mesopotamia",
+            b: "Egypt",
+            c: "China",
+            d: "Ancient Greece"
+        },
+        correctAnswer: 'a'
+    },
+    {
+        id:8,
+        question: "What was the Boston Tea Party?",
+        answerOptions: {
+            a: "A birthday party for the president",
+            b: "A political protest against British taxation in the American colonies",
+            c: "A movie",
+            d: "A theater play"
+        },
+        correctAnswer: 'b'
+    },
+    {
+        id:9,
+        question: "Which was the last state to join the US in 1959?",
+        answerOptions: {
+            a: "Alaska",
+            b: "California",
+            c: "Hawaii",
+            d: "Arizona"
+        },
+        correctAnswer: 'c'
+    },
+    {
+        id:10,
+        question: "The Statue of Liberty was a gift to the United States from which European country?",
+        answerOptions: {
+            a: "Germany",
+            b: "France",
+            c: "Spain",
+            d: "Italy"
+        },
+        correctAnswer: 'b'
+    }
+]
